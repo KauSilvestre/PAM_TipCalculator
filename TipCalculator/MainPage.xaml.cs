c34dcfd84325d160106058f8826493d50a95d43f
KauSilvestre <kaaueetec@gmail.com>
2024-08-23 21:52:23 -0300

    criação do projeto

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void setLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
